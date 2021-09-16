39a95392e17c5488aeccc8f8a0a4fb4eb089f96b
Juan Garcia <juan.garcia-ga@escuelaing.edu.co>
2021-09-15 22:56:29 -0500

    Branch seguridad

---
+++ src/main/java/edu/eci/ieti/proyect/controller/auth/AuthController.java
@@ -0,0 +1,11 @@
+package edu.eci.ieti.proyect.controller.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping( "v1/auth" )
+public class AuthController {
+
+
+}
--- src/main/java/edu/eci/ieti/proyect/controller/user/UserController.java
+++ src/main/java/edu/eci/ieti/proyect/controller/user/UserController.java
@@ -1,4 +1,4 @@
-package edu.eci.ieti.proyect.controller;
+package edu.eci.ieti.proyect.controller.user;
 
 
 
